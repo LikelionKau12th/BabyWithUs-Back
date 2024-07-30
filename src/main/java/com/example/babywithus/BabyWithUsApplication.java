8123ebb55ac29accd42165ca1b92182f9fc92c2e
jwaminseok <darth0324@naver.com>
2024-07-30 15:57:29 +0900

    feat/ WebConfig설정

---
+++ src/main/java/com/example/babywithus/BabyWithUsApplication.java
@@ -0,0 +1,13 @@
+package com.example.babywithus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BabyWithUsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BabyWithUsApplication.class, args);
+    }
+
+}
